3a20bbebb631a8a195b189f976cdc059a5f29b9e
Oman Ferrer <oman.ferrer@fluxit.com.ar>
2017-04-16 23:27:19 -0300

    lector excel

--- src/prueba/ReadExcel.java
+++ src/prueba/ReadExcel.java
@@ -19,43 +19,48 @@ public class ReadExcel {
         try {
         	
         	BufferedReader br = new BufferedReader(new FileReader(csvFile));
-        	boolean primero = true;
+        	
+        	line = br.readLine(); // Leo la primera fila con los nombres de las columnas
+        	line = br.readLine(); // Leo la primera pieza
+        	String pieza = "";
+            while (line != null) {
+            		
+        		String[] country = line.split(cvsSplitBy);
+                
+            	if(country[0].trim().length()>0) {
+            		pieza = country[0].trim();
+            		System.out.print("Creando la pieza: " + pieza);
+            		System.out.print("   Dato1: " + country[1].trim()); System.out.print("   Cant. Conj.: " + country[3].trim()); 
+            		System.out.print("   Longitud: " + country[6].trim()); System.out.print("   Peso: " + country[8].trim()); 
+            		System.out.print("   Pintura: " + country[9].trim()); System.out.println("   Descripcion: " + country[10].trim());
+            		
+            		line = br.readLine();
+            		
+            	} else if (country[2].trim().length()==0) { // OPCIONAL: se podría quitar este else si se borran los totales del excel
+            		System.out.println();
+            		System.out.println("                                                                                             Peso total: "+country[8].trim());
+            		System.out.println("   -----------------------------------------------------------------------------------------------------------------");
+            		
+            		line = br.readLine();
 
-            while ((line = br.readLine()) != null) {
-            	
-            	// La primera fila no se toma en cuenta
-            	if(!primero){
-            		
-            		String[] country = line.split(cvsSplitBy);
-                    
-                	if(country[0].trim().length()>0) {
-                		System.out.println("Creando una pieza... ");
-                		System.out.print("   Conjunto: " + country[0].trim());                		
-                		System.out.print("   Dato1: " + country[1].trim());
-                		System.out.print("   Cant. Conj.: " + country[3].trim());
-                		System.out.print("   Longitud: " + country[6].trim());
-                		System.out.print("   Peso: " + country[8].trim());
-                		System.out.print("   Pintura: " + country[9].trim());
-                		System.out.println("   Descripcion: " + country[10].trim());
-                		
-                	} else if (country[2].trim().length()>0) {
-            			System.out.println("     Creando una subpieza... ");
-            			System.out.print("        Parte: " + country[2].trim());                		
-                		System.out.print("        Cant.: " + country[4].trim());
-                		System.out.print("        Perfil: " + country[5].trim());
-                		System.out.print("        Longitud: " + country[6].trim());
-                		System.out.print("        Area: " + country[7].trim());
-                		System.out.println("        Peso: " + country[8].trim());
-                			
-                	} else {
-                		System.out.println();
-                		System.out.println("                                                                                             Peso total: "+country[8].trim());
-                		System.out.println("   -----------------------------------------------------------------------------------------------------------------");
-                	}
-            		
             	} else {
-            		primero = false;
+            		// Mientras encuentre subpiezas las proceso
+            		System.out.println("     Creando subpiezas para la pieza: " + pieza);
+	            	while (line != null && country[2].trim().length()>0) {
+	        			System.out.print("        Parte: " + country[2].trim());                		
+	            		System.out.print("        Cant.: " + country[4].trim());
+	            		System.out.print("        Perfil: " + country[5].trim());
+	            		System.out.print("        Longitud: " + country[6].trim());
+	            		System.out.print("        Area: " + country[7].trim());
+	            		System.out.println("        Peso: " + country[8].trim());
+	            		
+	            		line = br.readLine();
+	            		if(line!=null) 
+	            			country = line.split(cvsSplitBy);
+	            	}
+	            	
             	}
+            	
             }
             
             br.close();
